a1a362d491cb0dcbcbb9da054dccb32e83a304c6
Elvis de Freitas <edigitalb@gmail.com>
2018-02-20 23:20:35 -0300

    printing

--- src/main/java/com/mageddo/App.java
+++ src/main/java/com/mageddo/App.java
@@ -1,6 +1,5 @@
 package com.mageddo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,14 +11,9 @@ import org.springframework.core.env.Environment;
 @Configuration
 public class App {
 
-	@Autowired
-	private Environment environment;
-
 	public static void main(String[] args) {
-		SpringApplication.run(App.class).getBean(App.class).doTest();
+		final Environment env = SpringApplication.run(App.class).getBean(Environment.class);
+		System.out.printf("app.name=%s, a=%s, b=%s%n", env.getProperty("app.name"), env.getProperty("a"), env.getProperty("b"));
 	}
 
-	public void doTest(){
-		System.out.println("success");
-	}
 }
